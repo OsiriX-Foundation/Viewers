5fe766140f1a8edb2c59b57429edb26e6ee27dc0
gerome12 <gerome.pasquier@gmail.com>
2018-05-14 11:26:05 +0200

    Update docker_push.sh

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+ls
+
+
+#docker build --building-arg INSTALL_MONGO=true -t osirixfoundation/kheops-viewer .
+
+#docker push osirixfoundation/kheops-viewer
