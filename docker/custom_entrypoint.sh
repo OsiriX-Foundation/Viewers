5001cb1469813277115a7e2db9fd55d18c889ca4
gerome12 <gerome.pasquier@gmail.com>
2018-05-28 17:41:29 +0200

    Update custom_entrypoint.sh

--- docker/custom_entrypoint.sh
+++ docker/custom_entrypoint.sh
@@ -5,4 +5,7 @@ echo $OAuth2_secret
 export METEOR_SETTINGS="{\"servers\":{\"dicomWeb\":[{\"name\":\"DCM4CHEE\",\"wadoUriRoot\":\"$KHEOPS_WADO_URI_ROOT\",\"qidoRoot\":\"$KHEOPS_QIDO_ROOT\",\"authorizationRoot\":\"$KHEOPS_AUTHORIZATION_ROOT\",\"wadoRoot\":\"$KHEOPS_WADO_ROOT\",\"qidoSupportsIncludeField\":false,\"imageRendering\":\"wadouri\",\"thumbnailRendering\":\"wadors\",\"requestOptions\":{\"auth\":\"admin:admin\",\"logRequests\":true,\"logResponses\":false,\"logTiming\":true}}]},\"defaultServiceType\":\"dicomWeb\",\"dropCollections\":true,\"public\":{\"verifyEmail\":false,\"ui\":{\"studyListFunctionsEnabled\":true,\"leftSidebarOpen\":false,\"displaySetNavigationLoopOverSeries\":false,\"displaySetNavigationMultipleViewports\":true,\"autoPositionMeasurementsTextCallOuts\":\"TRLB\"}},\"proxy\":{\"enabled\":true},\"private\":{\"oAuth\":{\"google\":{\"clientId\":\"$KHEOPS_CLIENT_ID\",\"secret\":\"$OAuth2_secret\"}}}}"
 echo $METEOR_SETTINGS
 
+export ROOT_URL=$KHEOPS_ROOT_URL
+echo $ROOT_URL
+
 ./entrypoint.sh node main.js
