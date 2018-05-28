c87d284a4f7bd7fed41383b07b857125b22e9a98
gerome12 <gerome.pasquier@gmail.com>
2018-05-28 15:19:44 +0200

    Update custom_entrypoint.sh

--- docker/custom_entrypoint.sh
+++ docker/custom_entrypoint.sh
@@ -1,7 +1,7 @@
 
 
-export METEOR_SETTINGS="{\"servers\":{\"dicomWeb\":[{\"name\":\"DCM4CHEE\",\"wadoUriRoot\":\"$KHEOPS_wadoUriRoot\",\"qidoRoot\":\"$KHEOPS_qidoRoot\",\"authorizationRoot\":\"$KHEOPS_authorizationRoot\",\"wadoRoot\":\"$KHEOPS_wadoRoot\",\"qidoSupportsIncludeField\":false,\"imageRendering\":\"wadouri\",\"thumbnailRendering\":\"wadors\",\"requestOptions\":{\"auth\":\"admin:admin\",\"logRequests\":true,\"logResponses\":false,\"logTiming\":true}}]},\"defaultServiceType\":\"dicomWeb\",\"dropCollections\":true,\"public\":{\"verifyEmail\":false,\"ui\":{\"studyListFunctionsEnabled\":true,\"leftSidebarOpen\":false,\"displaySetNavigationLoopOverSeries\":false,\"displaySetNavigationMultipleViewports\":true,\"autoPositionMeasurementsTextCallOuts\":\"TRLB\"}},\"proxy\":{\"enabled\":true},\"private\":{\"oAuth\":{\"google\":{\"clientId\":\"$KHEOPS_clientId\",\"secret\":\"52Q-3JwelmKAn2CQKs5pd0Jr\"}}}}"
-ls
-echo custom_entrypoint
-echo $METEOR_SETTINGS
+#export METEOR_SETTINGS="{\"servers\":{\"dicomWeb\":[{\"name\":\"DCM4CHEE\",\"wadoUriRoot\":\"$KHEOPS_wadoUriRoot\",\"qidoRoot\":\"$KHEOPS_qidoRoot\",\"authorizationRoot\":\"$KHEOPS_authorizationRoot\",\"wadoRoot\":\"$KHEOPS_wadoRoot\",\"qidoSupportsIncludeField\":false,\"imageRendering\":\"wadouri\",\"thumbnailRendering\":\"wadors\",\"requestOptions\":{\"auth\":\"admin:admin\",\"logRequests\":true,\"logResponses\":false,\"logTiming\":true}}]},\"defaultServiceType\":\"dicomWeb\",\"dropCollections\":true,\"public\":{\"verifyEmail\":false,\"ui\":{\"studyListFunctionsEnabled\":true,\"leftSidebarOpen\":false,\"displaySetNavigationLoopOverSeries\":false,\"displaySetNavigationMultipleViewports\":true,\"autoPositionMeasurementsTextCallOuts\":\"TRLB\"}},\"proxy\":{\"enabled\":true},\"private\":{\"oAuth\":{\"google\":{\"clientId\":\"$KHEOPS_clientId\",\"secret\":\"52Q-3JwelmKAn2CQKs5pd0Jr\"}}}}"
+
+echo customcustom
+#echo $METEOR_SETTINGS
 ./entrypoint.sh
