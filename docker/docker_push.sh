035acab7f35240aea0f3fcc91429947ab27b4df9
gerome12 <gerome.pasquier@gmail.com>
2018-05-28 13:57:25 +0200

    [ci skip]

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -2,6 +2,8 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 mv Packages OHIFViewer/packages
+mv docker/Dockerfile OHIFViewer/Dockerfile
+mv docker/custom_entrypoint.sh OHIFViewer/custom_entrypoint.sh
 
 cd OHIFViewer
 docker build --build-arg INSTALL_MONGO=true -t osirixfoundation/kheops-viewer .
