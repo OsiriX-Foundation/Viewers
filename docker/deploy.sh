c75987e0cec81bd969365de8518a1006eb175191
gerome12 <gerome.pasquier@gmail.com>
2018-12-10 17:12:03 +0100

    [ci skip]

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,6 +3,8 @@
 ls 
 pwd
 
+mv ~/standaloneViewerBuild docker/standaloneViewerBuild
+
 docker build . -t="osirixfoundation/viewer"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
