77bb13da9e1e30120a07d045eddc0f48aeabb249
gerome12 <gerome.pasquier@gmail.com>
2018-12-10 18:39:52 +0100

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-ls 
-pwd
-
 mv /home/travis/standaloneViewerBuild docker/standaloneViewerBuild
-ls
-ls standaloneViewerBuild
-pwd
 cd docker
-ls
 docker build . -t="osirixfoundation/viewer"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
