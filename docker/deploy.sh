75a0147d7830d345bdc20dda87fc9f8a3a121497
gerome12 <gerome.pasquier@gmail.com>
2018-12-10 14:49:39 +0100

    Create deploy.sh

---
+++ docker/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ls 
+pwd
+
+docker build ./docker/ -t="osirixfoundation/viewer"
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push osirixfoundation/viewer
