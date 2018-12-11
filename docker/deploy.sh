e2d8c6c55b677f770c88e266376d21aa5a5e0acd
gerome12 <gerome.pasquier@gmail.com>
2018-12-11 14:18:22 +0100

    [ci skip]

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,6 +4,3 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build ./docker/ -t="osirixfoundation/viewer"
 docker push osirixfoundation/viewer
-
-docker build ./docker/ssh/ -t osirixfoundation/viewer:ssh
-docker push osirixfoundation/viewer:ssh
