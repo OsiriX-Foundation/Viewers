d085cc64c1383ce26163ab54ae1c47ff7ae95b5d
gerome12 <gerome.pasquier@gmail.com>
2018-12-11 14:46:35 +0100

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,6 +6,6 @@ docker build ./docker/ -t="osirixfoundation/viewer"
 docker push osirixfoundation/viewer
 
 
-chmod +x docker/ssh/deployssh.sh
+chmod +x docker/ssh/deployssl.sh
 mv $TRAVIS_BUILD_DIR/docker/standaloneViewerBuild $TRAVIS_BUILD_DIR/docker/ssh/standaloneViewerBuild
 ./docker/ssh/deployssl.sh
