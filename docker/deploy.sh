8df0dc7f8225a2bdadc16d4e5498a66e3b6d964f
gerome12 <gerome.pasquier@gmail.com>
2018-12-11 14:06:28 +0100

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,7 +2,7 @@
 
 docker build ./docker/ -t="osirixfoundation/viewer"
 
-docker build ./docker/ssh/ -t="osirixfoundation/viewer:ssh"
+docker build ./docker/ssh/ -t osirixfoundation/viewer:ssh
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
