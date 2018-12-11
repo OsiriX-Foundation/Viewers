f392b12b7b43b97c29aa0648330c5a3eb797fc27
gerome12 <gerome.pasquier@gmail.com>
2018-12-11 18:22:01 +0100

    no-tls

--- docker/deploy_none_tls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-docker build ./docker/ -t osirixfoundation/viewer:nonetls
-docker push osirixfoundation/viewer:nonetls
