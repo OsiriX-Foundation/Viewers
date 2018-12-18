682c33957d8962a6efca222b08e93c6fdbfe7b6e
Joël Spaltenstein <spalte@naturalimage.ch>
2018-12-18 11:25:13 +0100

    new env var names

--- docker/custom_entrypoint.sh
+++ docker/custom_entrypoint.sh
@@ -3,6 +3,6 @@
 OAuth2_secret=$(cat /run/secrets/OAuth2_secret)
  
 export METEOR_SETTINGS="{\"servers\":{\"dicomWeb\":[{\"name\":\"DCM4CHEE\",\"wadoUriRoot\":\"$KHEOPS_WADO_URI_ROOT\",\"qidoRoot\":\"$KHEOPS_QIDO_ROOT\",\"authorizationRoot\":\"$KHEOPS_AUTHORIZATION_ROOT\",\"wadoRoot\":\"$KHEOPS_WADO_ROOT\",\"qidoSupportsIncludeField\":false,\"imageRendering\":\"wadouri\",\"thumbnailRendering\":\"wadors\",\"requestOptions\":{\"auth\":\"admin:admin\",\"logRequests\":true,\"logResponses\":false,\"logTiming\":true}}]},\"defaultServiceType\":\"dicomWeb\",\"dropCollections\":true,\"public\":{\"verifyEmail\":false,\"ui\":{\"studyListFunctionsEnabled\":true,\"leftSidebarOpen\":false,\"displaySetNavigationLoopOverSeries\":false,\"displaySetNavigationMultipleViewports\":true,\"autoPositionMeasurementsTextCallOuts\":\"TRLB\"}},\"proxy\":{\"enabled\":true},\"private\":{\"oAuth\":{\"google\":{\"clientId\":\"$KHEOPS_CLIENT_ID\",\"secret\":\"$OAuth2_secret\"}}}}"
-export ROOT_URL=$KHEOPS_ROOT_URL
+export ROOT_URL=$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST
 
 ./entrypoint.sh node main.js
