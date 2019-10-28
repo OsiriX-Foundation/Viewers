1d6b085a105f6da30e0409ca10678a3866fa77ac
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2019-10-28 16:24:50 +0100

    no server and disable studylist view

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -2,21 +2,7 @@ window.config = {
   // default: '/'
   routerBasename: '/',
   extensions: [],
-  showStudyList: true,
-  servers: {
-    dicomWeb: [
-      {
-        name: 'DCM4CHEE',
-        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        qidoSupportsIncludeField: true,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        enableStudyLazyLoad: true,
-      },
-    ],
-  },
+  showStudyList: false,
   // Extensions should be able to suggest default values for these?
   // Or we can require that these be explicitly set
   hotkeys: [
